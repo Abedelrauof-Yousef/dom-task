a5e10c1c2245d03b03c36f18e114be5f787cd8f8
Abedelrauof <abedelrauofyousef@gmail.com>
2024-06-22 19:48:38 +0300

    Q2 solved

--- app.js
+++ app.js
@@ -1,25 +1,44 @@
-let unorderList = document.querySelector("ul");
-let listItem = document.createElement("li");
-let fruits = [listItem, listItem, listItem, listItem, listItem];
-let imgSrcs = [
-  "fruits-pictures/apple.webp",
-  "fruits-pictures/bannana.avif",
-  "fruits-pictures/mango.jpg",
-  "fruits-pictures/Orange.jpg",
-  "fruits-pictures/strawberry.avif",
-];
-unorderList.style.listStyle = "none";
-unorderList.style.marginLeft = "600px";
+// Q1
+
+// let unorderList = document.querySelector("ul");
+// let listItem = document.createElement("li");
+// let fruits = [listItem, listItem, listItem, listItem, listItem];
+// let imgSrcs = [
+//   "fruits-pictures/apple.webp",
+//   "fruits-pictures/bannana.avif",
+//   "fruits-pictures/mango.jpg",
+//   "fruits-pictures/Orange.jpg",
+//   "fruits-pictures/strawberry.avif",
+// ];
+// unorderList.style.listStyle = "none";
+// unorderList.style.marginLeft = "600px";
 
 
 
-for (let i = 0; i < fruits.length; i++) {
-    let img = document.createElement("img");
-    img.src = imgSrcs[i]
-    img.style.display = "block";
-    img.style.width ="200px";
-    fruits[i].appendChild(img)  
-    unorderList.appendChild(fruits[i])  
-}
+// for (let i = 0; i < fruits.length; i++) {
+//     let img = document.createElement("img");
+//     img.src = imgSrcs[i]
+//     img.style.display = "block";
+//     img.style.width ="200px";
+//     fruits[i].appendChild(img)
+//     unorderList.appendChild(fruits[i])  
+// }
 
 
+// Q2
+
+// function arraySearcher(array, numbers){
+//   for (let i = 0; i < array.length; i++){
+//       if(i === number){
+//           return(i);
+//       }
+//   }
+
+//   return(-1);
+// }
+
+
+// let arr = ["1", "2", "3", "4", "5"];
+// let number = 1;
+
+// console.log(arraySearcher(arr, number));
